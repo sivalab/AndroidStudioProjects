1ad1a96dc94bd68419d213812dd9fd4ec18fab3e
sivalab <80347389+sivalab@users.noreply.github.com>
2025-01-22 02:37:27 -0500

    Fixed all issues
    Fixed all issues

--- TipCalc/app/src/main/java/com/example/tipcalc/MainActivity.kt
+++ TipCalc/app/src/main/java/com/example/tipcalc/MainActivity.kt
@@ -1,29 +1,58 @@
 package com.example.tipcalc
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.semantics.Role.Companion.Button
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.tipcalc.ui.theme.TipCalcTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Get references to UI elements
+        val billAmountEditText: EditText = findViewById(R.id.ip_bill_amount)
+        val otDisplayResultTextView: TextView = findViewById(R.id.result_Display)
+
+        // get references to buttons
+        val tip15button: Button = findViewById(R.id.button_click_per15)
+        val tip18button: Button = findViewById(R.id.button_click_per18)
+        val tip20button: Button = findViewById(R.id.button_click_per20)
+        var result: String
+        var tipPerVal: Int
+        //  Set Click listeners for each button
+        tip15button.setOnClickListener {
+            tipPerVal = 15
+            result = calculateTip(this,billAmountEditText, tipPerVal )
+            otDisplayResultTextView.text = result
+        }
+
+        tip18button.setOnClickListener {
+            tipPerVal = 18
+            result = calculateTip(this,billAmountEditText, tipPerVal)
+            otDisplayResultTextView.text = result
+        }
+
+        tip20button.setOnClickListener {
+            tipPerVal = 20
+            result = calculateTip(this,billAmountEditText, tipPerVal)
+            otDisplayResultTextView.text = result
+        }
+
+
+
 //        enableEdgeToEdge()
 //        setContent {
 //            TipCalcTheme {
@@ -36,9 +65,6 @@ class MainActivity : ComponentActivity() {
 //           }
 //
 //        }
-        private void initTip15perButton () {
-            Button displayButton = findview
-        }
     }
 }
 
@@ -51,6 +77,29 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         )
     }
 }
+
+private fun calculateTip(context: Context,
+                         billAmountEditText: EditText,
+                         tipPercentage: Int
+): String {
+
+    val billAmountText =  billAmountEditText.text.toString()
+    //val tipPerVal: Int = tipPercentage
+
+    if (billAmountText.isNotEmpty()) {
+        val billAmount: Float = billAmountText.toFloat()
+        val tipAmount: Float  = billAmount * (tipPercentage.toFloat() / 100)
+        val totalAmount: Float = billAmount + tipAmount
+        return context.getString(R.string.result_Display, tipAmount, totalAmount)
+
+    }
+    else{
+        return "Please Enter Valid Amount"
+    }
+}
+
+
+
 
 @Preview(showBackground = true, name = "background_02.png", fontScale = 0.85f)
 @Composable
